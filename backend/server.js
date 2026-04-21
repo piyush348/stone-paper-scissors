a5f3fea8c5edb15ed922a9ed83abfcd4353e781d
Piyush Chaturvedi <piyush.2325mca1022@kiet.edu>
2026-04-22 01:39:15 +0530

    Initial project commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://test:test123@cluster0.qerlehb.mongodb.net/mygame?retryWrites=true&w=majority",
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("ERROR:", err));
+
+const GameSchema = new mongoose.Schema({
+  player1: String,
+  player2: String,
+  rounds: Array,
+  finalWinner: String,
+});
+
+const Game = mongoose.model("Game", GameSchema);
+
+app.post("/api/game", async (req, res) => {
+  const game = new Game(req.body);
+  await game.save();
+  res.send(game);
+});
+
+app.get("/api/games", async (req, res) => {
+  const data = await Game.find();
+  res.send(data);
+});
+
+app.listen(5000, () => console.log("Server running"));
